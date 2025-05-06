f1b99c4a8538b34d09d6d51a2643ee35a1f2d979
Imran Ahmad <ahmadimrankh99@gmail.com>
2025-05-06 16:07:42 +0500

    Committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import Hero from './Pages/Hero'
+import Bento from './Pages/Bento'  
+import Logos from './Pages/Logos'
+import Subscribe from './Pages/Subscribe'
 import './App.css'
 
 
@@ -9,6 +12,12 @@ function App() {
   return (
     <>
     <Hero/>
+    <hr />
+    <Bento/>
+    <hr />
+    <Logos/>
+    <hr />
+    <Subscribe/>
     </>
   )
 }
